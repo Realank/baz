ba3dd9dfacd1a5a96d1fb9a65428d32082d807f7
Realank <realank@126.com>
2015-12-21 15:35:43 +0800

    fix bugs

--- baz/iForeLoadingView2.m
+++ baz/iForeLoadingView2.m
@@ -7,7 +7,7 @@
 //
 #import "iForeLoadingView2.h"
 #define VIEW_SIZE 110.0
-#define ANIM_DURATION 2.5
+#define ANIM_DURATION 2
 #define ROUND_R 47.0
 #define CIRCLE_MAX_ANGLE (1.9*M_PI)
 #define CIRCLE_MIN_ANGLE (0.1*M_PI)
@@ -20,8 +20,8 @@ typedef NS_ENUM(NSInteger, StepSection) {
 };
 
 @interface iForeLoadingView2 ()
-@property (nonatomic,strong) UIView *loadingView;
-@property (nonatomic,strong) CADisplayLink *link;
+@property (nonatomic,weak) UIView *loadingView;
+@property (nonatomic,weak) CADisplayLink *link;
 @property (nonatomic,assign) CGFloat startAngle;
 @property (nonatomic,assign) CGFloat endAngle;
 @property (nonatomic,assign) StepSection section;
@@ -38,7 +38,25 @@ typedef NS_ENUM(NSInteger, StepSection) {
     return self;
 }
 
+- (void)dealloc{
+    
+    if (self.link) {
+        [self.link invalidate];
+        self.link = nil;
+    }
+    
+    if (self.animateLayer) {
+        [self.animateLayer removeFromSuperlayer];
+    }
+    
+    [self.loadingView removeFromSuperview];
+    self.loadingView.hidden = YES;
+    self.loadingView = nil;
+    [self removeFromSuperview];
+}
+
 - (void)updateLayout{
+    //    NSLog(@"refreshing");
     CGFloat angle = self.endAngle - self.startAngle;
     switch (self.section) {
         case STEP_INCREASE:{
@@ -69,15 +87,13 @@ typedef NS_ENUM(NSInteger, StepSection) {
     }
 }
 
-- (void)dealloc{
-    [self hideLoading];
-}
 
 - (void) showLoading{
-    self.loadingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, VIEW_SIZE, VIEW_SIZE)];
-    self.loadingView.backgroundColor = [UIColor clearColor];
-    self.loadingView.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-    [self addSubview:self.loadingView];
+    UIView *loadingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, VIEW_SIZE, VIEW_SIZE)];
+    loadingView.backgroundColor = [UIColor clearColor];
+    loadingView.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    [self addSubview:loadingView];
+    self.loadingView = loadingView;
     
     CGPoint center = CGPointMake(VIEW_SIZE/2, VIEW_SIZE/2);
     
@@ -86,7 +102,7 @@ typedef NS_ENUM(NSInteger, StepSection) {
     if (self.lineColor) {
         animateLayer.strokeColor = self.lineColor.CGColor;
     }else{
-        animateLayer.strokeColor = [UIColor grayColor].CGColor;
+        animateLayer.strokeColor = [UIColor lightGrayColor].CGColor;
     }
     
     animateLayer.fillColor = [UIColor clearColor].CGColor;
@@ -100,7 +116,7 @@ typedef NS_ENUM(NSInteger, StepSection) {
     iconView.alpha = 0;
     self.iconView = iconView;
     [UIView animateWithDuration:0.2 animations:^{
-        iconView.alpha = 1;
+        iconView.alpha = 0.5;
     }];
     iconView.center = center;
     [self.loadingView addSubview:iconView];
@@ -110,48 +126,50 @@ typedef NS_ENUM(NSInteger, StepSection) {
 }
 
 - (void)startAnimate{
-
+    
     self.section = STEP_INCREASE;
     self.startAngle = 0;
     self.endAngle = CIRCLE_MIN_ANGLE;
-    _link = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateLayout)];
-    //    _link.frameInterval = 2;
-    [_link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+    CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateLayout)];
+    [link addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    self.link = link;
     
 }
 
 - (void) endAnimate{
     self.section = STEP_ENDING;
+    
 }
 
 - (void) hideLoading {
-    self.section = STEP_END;
-    if (self.link) {
-        [self.link invalidate];
-        self.link = nil;
+    if (self) {
+        self.section = STEP_END;
+        if (self.link) {
+            [self.link invalidate];
+            
+            self.link = nil;
+        }
+        
+        if (self.animateLayer) {
+            [self.animateLayer removeFromSuperlayer];
+        }
+        
+        if (self && self.loadingView) {
+            __weak __typeof(self) weakSelf = self;
+            [UIView animateWithDuration:0.3 animations:^{
+                weakSelf.iconView.alpha = 0;
+            } completion:^(BOOL finished) {
+                [weakSelf.loadingView removeFromSuperview];
+                weakSelf.loadingView.hidden = YES;
+                weakSelf.loadingView = nil;
+                [weakSelf removeFromSuperview];
+            }];
+        }
     }
-    
-    if (self.animateLayer) {
-        [self.animateLayer removeFromSuperlayer];
-    }
-    
-    if (self && self.loadingView) {
-        __weak __typeof(self) weakSelf = self;
-        [UIView animateWithDuration:0.3 animations:^{
-            weakSelf.iconView.alpha = 0;
-        } completion:^(BOOL finished) {
-            [weakSelf.loadingView removeFromSuperview];
-            weakSelf.loadingView.hidden = YES;
-            weakSelf.loadingView = nil;
-            [weakSelf removeFromSuperview];
-        }];
-    }
-    
-    
 }
 
 - (void)increaseCircle{
-    NSLog(@"increaseCircle");
+    //    NSLog(@"increaseCircle");
     CGFloat angleStep = (CIRCLE_MAX_ANGLE-CIRCLE_MIN_ANGLE)/ANIM_DURATION/60;
     self.startAngle += angleStep;
     self.endAngle += 2*angleStep;
@@ -161,7 +179,7 @@ typedef NS_ENUM(NSInteger, StepSection) {
     
 }
 - (void)decreaseCircle{
-    NSLog(@"decreaseCircle");
+    //    NSLog(@"decreaseCircle");
     CGFloat angleStep = (CIRCLE_MAX_ANGLE-CIRCLE_MIN_ANGLE)/ANIM_DURATION/60;
     self.startAngle += 2*angleStep;
     self.endAngle += angleStep;
@@ -172,7 +190,7 @@ typedef NS_ENUM(NSInteger, StepSection) {
 }
 
 - (void)endingCircle{
-    NSLog(@"endingCircle");
+    //    NSLog(@"endingCircle");
     CGFloat angleStep = (CIRCLE_MAX_ANGLE-CIRCLE_MIN_ANGLE)/ANIM_DURATION/60;
     self.startAngle += 4*angleStep;
     self.endAngle += angleStep;
